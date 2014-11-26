b56ad58eec9ef95227799ec34b1aa31dfefdd65b
Kiope Gyzen <kiopelani@gmail.com>
2014-11-26 10:48:58 -0800

    fixed header link

---
+++ spec/javascripts/tweet.model.spec.js
@@ -0,0 +1,12 @@
+describe('Foo', function() {
+  it("hello", function() {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe('Tweet Model', function() {
+  it("does some shit", function () {
+    expect(4 + 3).toBe(7);
+  });
+});
+
